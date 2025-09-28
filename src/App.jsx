f02b3211a4e5b5c19384f7a5c859bea55f6d7e59
Hasboy15S <muhammadhasbi.t@gmail.com>
2025-09-28 21:10:24 +0700

    update project

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,21 @@ import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import About from './Components/About/About'
 import Particles from './Particles';
+import TargetCursor from './TargetCursor';
 import './App.css'
 const App = () => {
   return (
     <>
-    <Navbar/>
+    <div>
+      <TargetCursor 
+        spinDuration={2}
+        hideDefaultCursor={true}
+      />
+      <Navbar/>
     <Hero/>
     <About/>
+    </div>
+    
     </>
   )
 }
